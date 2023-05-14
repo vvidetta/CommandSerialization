78b4e39d369c22c49531a4e75817da551a3f2255
Vito Videtta <vvidetta@yahoo.co.uk>
2023-05-14 15:37:11 +0100

    STASH

--- Commandserialization/Commandserialization.cpp
+++ Commandserialization/Commandserialization.cpp
@@ -3,10 +3,146 @@
 
 #include "pch.h"
 #include <iostream>
+#include <tuple>
+
+class API
+{
+public:
+    virtual ~API() = default;
+    API(API const&) = delete;
+    API& operator=(API const&) = delete;
+    API(API&&) = delete;
+    API& operator=(API&&) = delete;
+
+    virtual int add(int a, int b) = 0;
+
+    virtual int subtract(int a, int b) = 0;
+
+    virtual void simple() = 0;
+
+protected:
+    API() = default;
+};
+
+class ISerializer
+{
+public:
+    virtual ~ISerializer() = default;
+    ISerializer(ISerializer const&) = delete;
+    ISerializer& operator=(ISerializer const&) = delete;
+    ISerializer(ISerializer&&) = delete;
+    ISerializer& operator=(ISerializer&&) = delete;
+};
+
+class concreteAPI final : public API
+{
+public:
+    int add(int a, int b) override
+    {
+        return a + b;
+    }
+
+    int subtract(int a, int b) override
+    {
+        return a - b;
+    }
+
+    void simple() override
+    {
+        printf("simple called\n");
+    }
+};
+
+class ICommand
+{
+public:
+    virtual ~ICommand() = default;
+    ICommand(ICommand const&) = delete;
+    ICommand& operator=(ICommand const&) = delete;
+    ICommand(ICommand&&) = delete;
+    ICommand& operator=(ICommand&&) = delete;
+
+    virtual void execute() = 0;
+
+    virtual bool hasResult() const noexcept = 0;
+
+    virtual std::string serializeResult(ISerializer const& serializer) = 0;
+protected:
+    ICommand() = default;
+};
+
+template <
+    typename R,
+    typename... Args
+>
+class APICommand : public ICommand
+{
+public:
+    using APIFunction = R(API::*)(Args...);
+
+    explicit APICommand(APIFunction func, API& api, Args&&... args)
+        : m_func{ func },
+        m_args{ std::make_tuple<std::reference_wrapper<API>, Args...>(std::ref(api), args...) }
+    { }
+
+    void execute() override
+    {
+        std::apply(m_func, m_args);
+    }
+
+    bool hasResult() const noexcept override
+    {
+        return false;
+    }
+
+    std::string serializeResult(ISerializer const& serializer) override
+    {
+        return {};
+    }
+
+private:
+    APIFunction m_func;
+    std::tuple<std::reference_wrapper<API>, Args...> m_args;
+};
+
+template <
+    typename... Args
+>
+class APICommand : public ICommand
+{
+public:
+    using APIFunction = void(API::*)(Args...);
+
+    explicit APICommand(APIFunction func, API& api, Args&&... args)
+        : m_func{ func },
+        m_args{ std::make_tuple<std::reference_wrapper<API>, Args...>(std::ref(api), args...) }
+    { }
+
+    void execute() override
+    {
+        std::apply(m_func, m_args);
+    }
+
+    bool hasResult() const noexcept override
+    {
+        return false;
+    }
+
+    std::string serializeResult(ISerializer const& serializer) override
+    {
+        return {};
+    }
+
+private:
+    APIFunction m_func;
+    std::tuple<std::reference_wrapper<API>, Args...> m_args;
+};
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+    auto api = concreteAPI{};
+    auto command = std::make_unique<APICommand<void>>(&API::simple, api);
+    command->execute();
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
